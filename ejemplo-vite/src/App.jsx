b40b65905f650b27802305bd4f118cb98df9b2e0
Andres J. Nazzari <andresnazzari@gmail.com>
2024-04-04 20:57:19 -0300

    Add multiple Navbar components in App.jsx

--- ejemplo-vite/src/App.jsx
+++ ejemplo-vite/src/App.jsx
@@ -3,14 +3,14 @@ import './Navbar.jsx'
 import Navbar from './Navbar.jsx'
 
 function App() {
-
-  
+ 
   console.log('App')
-
-
 
   return (
     <> 
+      <Navbar/> 
+      <Navbar/> 
+      <Navbar/> 
       <Navbar/> 
       <h1>Bienvenidos al curso</h1>
     </>
--- ejemplo-vite/src/Navbar.jsx
+++ ejemplo-vite/src/Navbar.jsx
@@ -1,7 +1,7 @@
 import "./navbar.css";
 
 const Navbar =()=>{
-
+    console.log('Navbar')
     return <nav>hola soy Navbar</nav>
 }
 
